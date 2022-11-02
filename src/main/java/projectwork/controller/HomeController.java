8f5660b3f9fc2655323e940ac684e12725293c95
ProtoR0 <Proto.Giovanny@gmail.com>
2022-11-02 18:01:23 +0100

    Push di fine giornata

---
+++ src/main/java/projectwork/controller/HomeController.java
@@ -0,0 +1,16 @@
+package projectwork.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@GetMapping
+	public String getPage(@RequestParam(name = "destinazione", required = false) String destinazione ) {
+		return null;
+	}
+}
