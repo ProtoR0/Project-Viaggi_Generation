a304433c74c765736d87d1c82b25cb61f7cbac02
LongoLogan <Nightwatcher@live.it>
2022-11-03 17:49:25 +0100

    import in eccesso asportato
    account in questo controller non viene richiamato

--- src/main/java/projectwork/controller/AAdminController.java
+++ src/main/java/projectwork/controller/AAdminController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import projectwork.model.Account;
 import projectwork.service.AccountService;
 import projectwork.service.RecensioneService;
 
