4ddecc61183394345f4f2d12cb82d7fe8668004c
Tommaso995 <tommaso.magliocca@gmail.com>
2022-11-03 10:03:39 +0100

    Aggiunta area_admin,2,ara_free,area_utente

--- src/main/webapp/static/javascript/index.js
+++ src/main/webapp/static/javascript/index.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
 
    
-
     $(".aside").hover(function(){
 
-        $(this).css("width","325px");
-    });
-
-    $("input").focus(function(){
-        $(this).css("background-color","#cccccc");
+        $(this).css("width","350px")
     });
 
 
