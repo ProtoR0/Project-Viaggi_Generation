0adcdc24d6bb6c0b6778fd7026e43b2827276543
Tommaso995 <tommaso.magliocca@gmail.com>
2022-11-03 09:14:10 +0100

    Aggiornamenti
    Css,JS

--- src/main/webapp/static/javascript/index.js
+++ src/main/webapp/static/javascript/index.js
@@ -4,7 +4,11 @@ $(document).ready(function(){
 
     $(".aside").hover(function(){
 
-        $(this).css("width","350px")
+        $(this).css("width","325px");
+    });
+
+    $("input").focus(function(){
+        $(this).css("background-color","#cccccc");
     });
 
 
