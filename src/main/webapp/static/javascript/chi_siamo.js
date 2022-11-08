2e8047f881b3c0169bd05c70a0673b2eba336c6c
DamianoL-21 <damin.9@hotmail.it>
2022-11-08 10:14:57 +0100

    Aggiunta di pagine chi siamo

---
+++ src/main/webapp/static/javascript/chi_siamo.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".aside").mouseenter(function(){
+        $(this).css("width","250px")
+    });
+        $(".aside").mouseleave(function(){
+            $(this).css("width","200px")
+    });
+
+});
