cdc04d8dcfd95f87bfc6d5483a772065a7e74f2f
DamianoL-21 <damin.9@hotmail.it>
2022-11-10 17:23:59 +0100

    Aggiornamento pagine chi siamo e lavora con noi

--- src/main/webapp/static/javascript/lavora_con_noi.js
+++ src/main/webapp/static/javascript/lavora_con_noi.js
@@ -15,3 +15,11 @@ $(document).ready(function(){
     });
 
 });
+
+window.addEventListener("load", sponsor);
+function sponsor (){
+let sponsor = ["/Project-Viaggi_Generation/static/img/Tripadvisor.jpg", "/Project-Viaggi_Generation/static/img/viaggi.jpg", "/Project-Viaggi_Generation/static/img/tramonto.jpg"];
+let casuale = Math.floor(Math.random()*3);
+document.getElementById("banner").innerHTML = '<img src="' + sponsor[casuale] + '" style="width: 600px; height: 125px; border-radius: 5px;" />';
+setTimeout("sponsor()", 500);
+}
