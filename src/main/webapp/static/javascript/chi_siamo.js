9ff909534b0b9b8f03ce89da146bd4fc9a64e85f
Tommaso995 <tommaso.magliocca@gmail.com>
2022-11-10 11:03:42 +0100

    Aggiornamenti

--- src/main/webapp/static/javascript/chi_siamo.js
+++ src/main/webapp/static/javascript/chi_siamo.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     $(".aside").mouseenter(function(){
-        $(this).css("width","250px")
+        $(this).animate({
+            right:"1px",
+            width:"250px",
+            height:"auto"
+        });
     });
         $(".aside").mouseleave(function(){
-            $(this).css("width","200px")
+            $(this).animate({
+                left:"1px",
+                width:"200px",
+                height:"auto"
+            });
     });
 
 });
