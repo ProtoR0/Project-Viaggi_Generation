93fd7c1c4e4b29e18462c2bf7319b491db344db3
Tommaso995 <tommaso.magliocca@gmail.com>
2022-11-11 10:09:13 +0100

    Aggiunta orologio navbar

--- src/main/webapp/static/javascript/area_utente.js
+++ src/main/webapp/static/javascript/area_utente.js
@@ -2,3 +2,26 @@ document.getElementById("dataInizio").onchange = function () {
     var input = document.getElementById("dataFine");
     input.setAttribute("min", this.value);
 }
+
+
+
+function zero(numero) {
+    if (numero<10) {
+        numero = "0" + numero;
+    }
+
+    return numero;
+}
+window.addEventListener("load", data);
+function data() {
+
+    let data = new Date();
+
+    let ora = zero(data.getHours());
+    let minuti = zero(data.getMinutes());
+    let secondi = zero(data.getSeconds());
+
+document.getElementById("orologio").innerHTML = (ora + ":" + minuti + ":" + secondi);
+setTimeout("data()", 1000);
+
+}
--- src/main/webapp/static/javascript/index.js
+++ src/main/webapp/static/javascript/index.js
@@ -31,3 +31,25 @@ $(document).ready(function(){
     });
 
 });
+
+
+function zero(numero) {
+    if (numero<10) {
+        numero = "0" + numero;
+    }
+
+    return numero;
+}
+window.addEventListener("load", data);
+function data() {
+
+    let data = new Date();
+
+    let ora = zero(data.getHours());
+    let minuti = zero(data.getMinutes());
+    let secondi = zero(data.getSeconds());
+
+document.getElementById("orologio").innerHTML = (ora + ":" + minuti + ":" + secondi);
+setTimeout("data()", 1000);
+
+}
