e014daab7ae2ce7c13c44e4bd3756afa9deb7904
DamianoL-21 <damin.9@hotmail.it>
2022-11-11 10:37:47 +0100

    Aggiornamenti pagine e fix area admin

--- src/main/webapp/static/javascript/lavora_con_noi.js
+++ src/main/webapp/static/javascript/lavora_con_noi.js
@@ -16,15 +16,6 @@ $(document).ready(function(){
 
 });
 
-window.addEventListener("load", sponsor);
-function sponsor (){
-let sponsor = ["/Project-Viaggi_Generation/static/img/mare.jpg", "/Project-Viaggi_Generation/static/img/grecia.jpg", "/Project-Viaggi_Generation/static/img/woman.jpg","/Project-Viaggi_Generation/static/img/tropicalel.jpg"];
-let casuale = Math.floor(Math.random()*3);
-document.getElementById("banner").innerHTML = '<img src="' + sponsor[casuale] + '" style="width: 100%; height: 125px; border-radius: 10px;" />';
-setTimeout("sponsor()", 500);
-}
-
-
 function zero(numero) {
     if (numero<10) {
         numero = "0" + numero;
